5042f3d1c9d13ee4e5a9755a91afeb6a1a8d228c
cmmclaug <cmmclaug@gmail.com>
2020-11-21 13:08:22 -0800

    Update preprocess to use docopt

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,41 +1,26 @@
+"""
+Retrieve datasets from a URL, file of URLs, or GitHub directory URL
+
+Usage: preprocess_data.py -i=<input> -o=<output> [-v]
+
+Options:
+-i <input>, --input <input>     Local raw data csv file directory
+-o <output>, --output <output>  Local output filename and path for preprocessed csv
+[-v]                            Report verbose output of dataset retrieval process
+"""
+import os
+import glob
+import pandas as pd
+from docopt import docopt
+args = docopt(__doc__)
+
+
 def preprocess_data():
-    """
-    Clean the raw local csv files before processing
-
-    Parameters
-    ----------
-    -i/--input  : string
-        Local raw data csv file directory
-    -o/--output : string
-        Local output filename and path for preprocessed csv
-
-    Options
-    -v/--verbose    : boolean
-        Report verbose output of dataset retrieval process
-
-    Examples
-    --------
-    python preprocess_data.py -i ../data/raw -o ../data/processed/clean_characters.csv -v
-    """
-    import os
-    import glob
-    import argparse
-    import pandas as pd
-
     # Setup script argument parsing
-    ap = argparse.ArgumentParser()
-
-    ap.add_argument("-i", "--input", required=True, 
-        help="input raw csv directory path")
-    ap.add_argument("-o", "--output", required=True, 
-        help="output csv filename and path")
-    ap.add_argument("-v", "--verbose", required=False, action='store_true',
-        help="Print script output (Optional)")
-    args = vars(ap.parse_args())
-
-    input_dir = args["input"]
-    output_filename = args["output"]
-    verbose = args["verbose"]
+    print(args)
+    input_dir = args["--input"]
+    output_filename = args["--output"]
+    verbose = args["-v"]
 
     assert os.path.exists(input_dir), "Invalid input directory path provided"
     if not os.path.exists(os.path.dirname(output_filename)):
