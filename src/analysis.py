fd07e6558a678e7b7e9481efbc68519a660f78ce
Aidan Mattrick <aidan.mattrick@gmail.com>
2020-11-28 11:54:01 -0800

    Updated analysis.py per PR comments

--- src/analysis.py
+++ src/analysis.py
@@ -1,11 +1,15 @@
 """
+Author: Aidan Mattrick
+
+Date: Nov 28, 2020
+
 This script trains models and output results in the form of a figure to be used for further analysis.
 
 Usage: analysis.py -i=<input> -o=<output> [-v]
 
 Options:
--i <input>, --input <input>     Local raw data csv file directory
--o <output>, --output <output>  Local output filename and path for preprocessed csv
+-i <input>, --input <input>     Local processed training data csv file in directory
+-o <output>, --output <output>  Local output directory for created pngs
 [-v]                            Report verbose output of dataset retrieval process
 """
 
@@ -13,6 +17,7 @@ import os
 import re
 import sys
 from hashlib import sha1
+import pickle
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -55,9 +60,8 @@ from vega_datasets import data
 from docopt import docopt
 from pylab import savefig
 from render_table import render_table
-
-print("start")
 args = docopt(__doc__)
+
 
 def read_input_file(input_file_path):
     """
@@ -123,26 +127,32 @@ def store_cross_val_results(model, scores, results):
 
 
 
-def train_model(dataframe):
+def train_models(train_df, models, param_grid=None):
     """
     Processes the data, trains the model, and returns a dataframe showing the results
 
     Parameters
     ----------
-    dataframe : pd.Dataframe
+    train_df : pd.Dataframe
         Dataframe to be used in model
+
+    models : dict
+        models to be trained
+
+    param_grid : dict
+        hyperparameters for model
 
     Returns
     ----------
         pandas.Dataframe
 
     """
-    train_df = dataframe
     X_train = train_df.drop(columns=['align'])
     y_train = train_df['align']
 
     numeric_features = ['appearances']
-    categorical_features = ['name', 'id', 'eye', 'hair', 'year', 'publisher', 'sex']
+    categorical_features = ['name', 'id', 'eye', 'hair', 'publisher', 'sex']
+    ordinal_features = ['year']
     drop_features = ['gsm', 'first_appearance']
 
     numeric_transformer = Pipeline(
@@ -159,27 +169,51 @@ def train_model(dataframe):
         ]
     )
 
+    #Fixing year levels for ordinal encoding
+    year_levels = set(train_df['year'].unique())
+    year_levels = list(year_levels)
+    #Cutting out NaN value
+    year_levels = year_levels[1:]
+
+    ordinal_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy='most_frequent')),
+            ("ordinal", OrdinalEncoder(categories=[year_levels], dtype=float))
+        ]
+    )
+
     preprocessor = make_column_transformer(
         ("drop", drop_features),
         (numeric_transformer, numeric_features),
         (categorical_transformer, categorical_features),
+         (ordinal_transformer, ordinal_features)
     )
 
     results_df = {}
 
-    models = {
-        "Dummy Classifier": DummyClassifier(),
-        "Decision Tree Classifier": DecisionTreeClassifier(),
-        "Random Forest Classifier": RandomForestClassifier(),
-        "SVM": SVC(),
-        "OVR LogisticRegression": OneVsRestClassifier(LogisticRegression()),
-        "LogisticRegression": LogisticRegression(),
-    }
+    if param_grid:
+        for key, value in models.items():
+            random_forest_pipeline = Pipeline(
+                steps=[
+                    ("preprocessor", preprocessor),
+                    (key, value),
+                    ]
+            )
+
+            random_search = RandomizedSearchCV(random_forest_pipeline, param_distributions=param_grid, cv=5, n_jobs=-1, n_iter=20, return_train_score=True)
+            random_search.fit(X_train, y_train)
+
+        results = pd.DataFrame(random_search.cv_results_).set_index("rank_test_score").sort_index()
+        results.reset_index(inplace=True)
+        results = results.rename(columns = {'index':'Ranked Test Scores'})
+        return results
+
 
     for key, value in models.items():
         pipeline = Pipeline(
-                steps=[("preprocessor", preprocessor),
-               (key, value)
+                steps=[
+                    ("preprocessor", preprocessor),
+                    (key, value)
                 ]
             )
         scores = cross_validate(pipeline, X_train, y_train, return_train_score=True, n_jobs=-1, verbose=1)
@@ -191,29 +225,53 @@ def train_model(dataframe):
     return results
 
 
-
 def save_img(data_frame, output_folder, file_name):
+    data_frame.to_pickle(output_folder + "/tables/" + file_name + ".pkl")
     fig_1, ax_1 = render_table(data_frame, header_columns=0, col_width=5)
     fig_1.savefig(output_folder + "/" + file_name)
 
+def save_img_large(data_frame, output_folder, file_name):
+    data_frame.to_pickle(output_folder + "/tables/" + file_name + ".pkl")
+    fig_1, ax_1 = render_table(data_frame, header_columns=0, col_width=8)
+    fig_1.savefig(output_folder + "/" + file_name)
+
 def main(input_file_path, output_folder_path):
-    print(input_file_path)
+    if verbose: print(input_file_path)
     data_frame = read_input_file(input_file_path)
-    print("Generating")
-    #process
-    # preprocessor = process_data(data_frame)
-    # print("processing data for model")
-    #train
-    model_df = train_model(data_frame)
-    print("Trained model")
+    if verbose: print("Generating")
+
+    #train_data
+    if verbose: print("Generating training data")
+    train_df = data_frame
+
+    #Run multiple models to select the best one
+    if verbose: print("Training model(s)")
+    models = {
+        "Dummy Classifier": DummyClassifier(),
+        "Decision Tree Classifier": DecisionTreeClassifier(),
+        "Random Forest Classifier": RandomForestClassifier(),
+        "SVM": SVC(),
+        "OVR LogisticRegression": OneVsRestClassifier(LogisticRegression()),
+        "LogisticRegression": LogisticRegression(),
+    }
+    model_df = train_models(train_df, models)
+    if verbose: print("Trained model(s)")
     save_img(model_df, output_folder_path, "Table_03")
-    print("Done")
+
+    #Performing hyperparameter optimization on best one
+    if verbose: print("Performing hyperparameter optimization on best model")
+    models = {
+        "Random Forest Classifier": RandomForestClassifier(),
+    }
+    param_grid = {"Random Forest Classifier__max_depth": 10.0 ** np.arange(-10, 10)}
+    model_df = train_models(train_df, models, param_grid)
+    if verbose: print("Trained model(s)")
+    save_img_large(model_df, output_folder_path, "Table_04")
+    if verbose: print("Done")
 
 
 if __name__ == "__main__":
-    print("101")
+    verbose = args["-v"]
     input_file = args["--input"]
     output_dir = args["--output"]
-    verbose = args["-v"]
-    print("102")
     main(input_file, output_dir)
